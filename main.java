31ade044af63add561d58beb7f931384263ac8bd
adarsh salgaonkar <adarshsalgaonkaras@gmail.com>
2026-03-05 00:14:17 +0530

    Operators Java

--- main.java
+++ main.java
@@ -1,11 +1,22 @@
-public class main {
-    public static void main(String[] args){
-        int x = 8;
-        int y = 2;
-        System.out.println(x);
-        System.out.println(x+y);
-        System.out.println(x*y);
-        System.out.println(x/y);
+// public class main {
+//     public static void main(String[] args){
+//         int x = 8;
+//         int y = 2;
+//         System.out.println(x);
+//         System.out.println(x+y);
+//         System.out.println(x*y);
+//         System.out.println(x/y);
 
+//     }
+// }
+
+// insted of above code you can write like this
+
+public class main {
+    public static void main(String[] args) {
+        int x =  5, y = 3, z = 2;
+        System.out.println(x);
+        System.out.println(y+1000);
+        System.out.println(z);
     }
 }
---
+++ operator.java
@@ -0,0 +1,22 @@
+public class operator {
+    public static void main(String [] args){
+        int x = 5;
+        System.out.println(x);
+        x--; // x-- means x = x - 1     -- This is decrement Operator.
+        System.out.println(x);
+        x++; // x++ means x = x + 1      -- This is increment Operator.
+        System.out.println(x);
+
+        ++x; // ++x means also same like x++ ()
+        System.out.println(x); 
+        --x; // ++x means also same like x-- ()
+        System.out.println(x); 
+
+
+        // Commments in JAVA
+        // This is single line comment.
+        /*  */  //This is multiline comment.
+        // Ctrl + / This is for selected lines comment.
+    }
+    
+}
